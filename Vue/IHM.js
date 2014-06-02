9a8894ca5bfa7633a83fbb088456ef176a63d1f8
JudeSeide <jude.seide@gmail.com>
2014-06-02 18:45:37 -0400

    modif de l'interface

--- Vue/IHM.js
+++ Vue/IHM.js
@@ -1,64 +1,64 @@
 function IHM() {
 
     var instance;
-    
-    this.updateTempo = function(){
+
+    this.updateTempo = function() {
         var horloge = Horloge.getInstance();
         var f_un = horloge.getFrequenceUn();
         var f_deux = horloge.getFrequenceDeux();
-        
+
         this.ledUn.clignoterUn(f_un);
         this.ledDeux.clignoterDeux(f_deux);
-        
+
         var metronome = Metronome.getInstance();
         var tempo = metronome.getTempo();
         var ecran = Ecran.getInstance();
         ecran.afficherTempo(tempo);
-        
+
         var son = HautParleur.getInstance();
         son.sonner(f_un);
     }
-    
-    this.updateMesure = function() {                
-        var horloge = Horloge.getInstance();        
-        var f_deux =  horloge.getFrequenceDeux();
-        
+
+    this.updateMesure = function() {
+        var horloge = Horloge.getInstance();
+        var f_deux = horloge.getFrequenceDeux();
+
         this.ledDeux.clignoterDeux(f_deux);
-        
+
         var metronome = Metronome.getInstance();
         var mesure = metronome.getMesure();
         var ecran = Ecran.getInstance();
         ecran.afficherMesure(mesure);
     }
-    
-    this.updateStart = function(){
+
+    this.updateStart = function() {
         var metronome = Metronome.getInstance();
         var started = metronome.isStarted();
-        
+
         var ecran = Ecran.getInstance();
         var son = HautParleur.getInstance();
-        
+
         if (started === false) {
             ecran.afficherTempo("");
             ecran.afficherMesure("");
             this.ledUn.clignoterUn(0);
-            this.ledDeux.clignoterDeux(0);            
+            this.ledDeux.clignoterDeux(0);
             son.sonner(0);
         } else {
             var horloge = Horloge.getInstance();
             var f_un = horloge.getFrequenceUn();
             var f_deux = horloge.getFrequenceDeux();
-        
+
             this.ledUn.clignoterUn(f_un);
             this.ledDeux.clignoterDeux(f_deux);
-        
+
             var tempo = metronome.getTempo();
             var mesure = metronome.getMesure();
             ecran.afficherMesure(mesure);
             ecran.afficherTempo(tempo);
-            
+
             var volume = metronome.getVolume();
-            son.sonner(f_un);            
+            son.sonner(f_un);
             son.reglerVolume(volume);
         }
 
@@ -71,21 +71,17 @@ function IHM() {
 
         son.reglerVolume(volume);
     };
-    
-    this.updateLang = function(){
+
+    this.updateLang = function() {
         var metronome = Metronome.getInstance();
         var lang = metronome.getLang();
-        
-        if(lang === "eng"){
-            $("#start").html("Start");
-            $("#stop").html("Stop");
+
+        if (lang === "eng") {
             $('#mesure').html("Measure");
-            
-        }else if(lang === "fr"){
-            $("#start").html("Allumer");
-            $("#stop").html("Arreter");
+
+        } else if (lang === "fr") {
             $('#mesure').html("Mesure");
-        } 
+        }
     }
 
     this.charger = function() {
@@ -96,8 +92,8 @@ function IHM() {
 
             //---- Volume
             $(function() {
-                var slider = $('.slider'),
-                        tooltip = $('.tooltip');
+                var slider = $('#volumeSlider'),
+                        tooltip = $('#volumeTooltip');
 
                 tooltip.hide();
 
@@ -142,17 +138,51 @@ function IHM() {
             });
 
             // ------ Tempo
-            $("#reglageTempo").slider();
             $(function() {
-                $('#reglageTempo').slider({
+                var slider = $('#tempoSlider'),
+                        tooltip = $('#tempoTooltip');
+
+                tooltip.hide();
+
+                slider.slider({
+                    //Config
+                    range: "min",
                     min: 40,
                     max: 208,
-                    slide: function(event, ui) {
+                    value: 40,
+                    start: function(event, ui) {
+                        tooltip.fadeIn('fast');
+                    },
+                    //Slider Event
+                    slide: function(event, ui) { //When the slider is sliding
                         var molette = new CommandeMoletteTempo(ui.value);
                         molette.execute();
 
-                    }
+                        var value = slider.slider('value'),
+                                tempo = $('.tempo');
+
+                        tooltip.css('left', value).text(ui.value);  //Adjust the tooltip accordingly
+
+                        if (value <= 5) {
+                            tempo.css('background-position', '0 0');
+                        }
+                        else if (value <= 25) {
+                            tempo.css('background-position', '0 -25px');
+                        }
+                        else if (value <= 75) {
+                            tempo.css('background-position', '0 -50px');
+                        }
+                        else {
+                            tempo.css('background-position', '0 -75px');
+                        }
+                        ;
+
+                    },
+                    stop: function(event, ui) {
+                        tooltip.fadeOut('fast');
+                    },
                 });
+
             });
 
             //----- Mesure 
@@ -176,13 +206,13 @@ function IHM() {
                 var cmdStop = new CommandeStop();
                 cmdStop.execute();
             });
-            
+
             //------Langue
             $("#languefr").click(function(e) {
                 var cmdfr = new CommandeLang("fr");
                 cmdfr.execute();
             });
-            
+
             $("#langueen").click(function(e) {
                 var cmdeng = new CommandeLang("eng");
                 cmdeng.execute();
